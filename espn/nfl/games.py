6c2ab6aee3a2faca219bd6d6e12400c6085e1fb8
Zach Stocker <zacharystocker@gmail.com>
2025-02-25 01:38:01 -0600

    added todo

--- espn/nfl/games.py
+++ espn/nfl/games.py
@@ -1,2 +1,4 @@
 # todo write code to pull game data from a box score
 
+# http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/401671889?lang=en&region=us
+# game id is the variable
