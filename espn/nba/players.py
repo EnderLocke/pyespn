56f51b2f8af8d91c87a42758e2082941bb0a09f5
Zach Stocker <zacharystocker@gmail.com>
2025-02-25 02:05:22 -0600

    adding todos

---
+++ espn/nba/players.py
@@ -0,0 +1,4 @@
+# todo add athlete stats and info
+#  stats
+# http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2025/athletes/4277905?lang=en&region=us
+
---
+++ espn/nba/teams.py
@@ -0,0 +1,3 @@
+# todo add team info api
+
+# http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2025/teams/1?lang=en&region=us
