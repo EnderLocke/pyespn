f09530036fb31b705925f4827792b8c113b9ef75
Zach Stocker <zacharystocker@gmail.com>
2025-02-24 23:08:29 -0600

    added func to get team data for a single season

--- espn/nfl/teams.py
+++ espn/nfl/teams.py
@@ -1,2 +1,9 @@
 # todo a func for getting team stats by year//
 #  build out a lookup table for a team name its not many lines
+import requests
+
+
+def get_season_team_stats(season, team):
+    url = f'http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/{season}/types/2/teams/{team}/statistics?lang=en&region=us'
+    response = requests.get(url)
+    return response.content
