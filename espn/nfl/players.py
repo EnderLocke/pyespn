c6d91dc38849d697bc2c7f9ef5ccf3abeb1be565
Zach Stocker <zacharystocker@gmail.com>
2025-02-24 21:49:42 -0600

    working on init player api

---
+++ espn/nfl/players.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_nfl_player_ids():
+    all_players = []
+    nfl_ath_url = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/athletes?lang=en&region=us'
+    response = requests.get(nfl_ath_url)
+    num_pages = response['data']['entries']
+
+    for i in range(1, num_pages):
+        page_url = nfl_ath_url + f'&page={i}'
+        page_response = requests.get(page_url)
+        for athlete in page_response:
+            if athlete['$ref']:
+                athlete_response = requests.get(athlete['$ref'])
+                athlete_data = {'id': athlete_response.data.id,
+                                'name': athlete_data.data.full_name}
+
+
+def get_player_stats(player_id):
+    stat_log_url = f'http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/athletes/{player_id}/statisticslog?lang=en&region=us'
+    log_response = requests.get(stat_log_url)
+
+
