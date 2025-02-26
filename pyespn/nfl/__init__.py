de8756e6adeee5a6c7af8640fd81bf21c46a4ab0
Zach Stocker <zacharystocker@gmail.com>
2025-02-25 23:54:46 -0600

    added get player ids to init

--- pyespn/nfl/__init__.py
+++ pyespn/nfl/__init__.py
@@ -1,4 +1,4 @@
-from .players import get_player_stat_urls, extract_stats_from_url, get_player_info
+from .players import get_player_stat_urls, extract_stats_from_url, get_player_info, get_nfl_player_ids
 from .teams import get_season_team_stats
 from .orchestration import get_nfl_players_historical_stats
 from .betting import get_team_year_ats, get_team_year_ml
