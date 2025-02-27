ca745d44a5af6b6a94a4738d52586b3c28d5bd7f
Zach Stocker <zacharystocker@gmail.com>
2025-02-26 21:07:05 -0600

    finished nfl test for team id

--- tests/nfl/test_nfl_teams.py
+++ tests/nfl/test_nfl_teams.py
@@ -4,15 +4,17 @@ import requests
 import json
 
 
-def check_nfl_team_id():
+def get_random_nfl_team_data():
     random_id = random.randint(1, len(nfl_teams_data) - 2)
     api_url = f'http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams/{random_id}'
     selected_team = next((team for team in nfl_teams_data if team["team_id"] == random_id), None)
     response = requests.get(api_url)
     content = json.loads(response.content)
 
-    assert selected_team['team_abbv'] == content['abbreviation']
+    return selected_team, content
 
 
+def test_nfl_team_ids():
+    local_team_data, api_team_data = get_random_nfl_team_data()
 
-
+    assert local_team_data['team_abbv'] == api_team_data['abbreviation']
