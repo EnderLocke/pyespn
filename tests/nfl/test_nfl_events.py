629b4684699577853ee7245c6b9ed364875a4476
Zach Stocker <zacharystocker@gmail.com>
2025-02-26 22:33:11 -0600

    added test case for events

--- tests/nfl/test_nfl_events.py
+++ tests/nfl/test_nfl_events.py
@@ -1,5 +1,6 @@
-from pyespn.nba.data import nba_teams_data
-from pytest_check import check
+from pyespn.nfl import get_game_info
+import pytest
+
 import requests
 import json
 
@@ -12,13 +13,19 @@ test_event_ids = [
     }
 ]
 
-def get_nfl_event_info():
-    pass
+
+def get_nfl_event_info(id):
+    content = get_game_info(id)
+    return content
 
 
+@pytest.mark.parametrize("test_case", test_event_ids)
+def test_nfl_events(test_case):
+    content = get_nfl_event_info(test_case['id'])
+    assert content['short_name'] == test_case['short_name']
+    assert content['name'] == test_case['name']
+    assert content['time'] == test_case['time']
 
 
-def test_nfl_events():
-    pass
 
 
