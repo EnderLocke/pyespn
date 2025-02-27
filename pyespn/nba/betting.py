25172d5efb0712225cbadb8a607bd1b2648c3c0d
Zach Stocker <zacharystocker@gmail.com>
2025-02-27 13:30:57 -0600

    adding futures

--- pyespn/nba/betting.py
+++ pyespn/nba/betting.py
@@ -1,6 +1,8 @@
 # todo add any betting apis here
 
 # http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2023/futures?lang=en&region=us
+from pyespn.nba.data import nba_teams_data
+from pyespn.utilities import get_team_id
 import requests
 import json
 
@@ -8,6 +10,16 @@ import json
 def _get_futures_year(year):
     url = f'http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/{year}/futures?lang=en&region=us'
     response = requests.get(url)
+    content = json.loads(response.content)
+    return content
+
+
+def _get_type_futures(data, futures_type):
+    try:
+        result = next(item for item in data["items"] if item["name"] == futures_type)
+    except StopIteration:
+        result = None
+    return result
 
 
 def _get_type_ats(data, ats_type):
@@ -25,11 +37,35 @@ def _get_team_year_ats(team_id, season):
     return content
 
 
+def get_year_nba_champ_futures(season, provider="DraftKings"):
+    content = _get_futures_year(season)
+
+    nba_futures = _get_type_futures(data=content,
+                                    futures_type='NBA - Winner')
+
+    provider_futures = next(future for future in nba_futures['futures'] if future['provider']['name'] == provider)
+
+    futures_list = []
+    for item in provider_futures['books']:
+        team_id = get_team_id(item['team']['$ref'])
+        result = next(team for team in nba_teams_data['teams'] if team['team_id'] == team_id)
+
+        item_dict = {
+            'team_name': result['team_name'],
+            'team_city': result['team_city'],
+            'champion_future': item['value']
+        }
+        futures_list.append(item_dict)
+
+    
+    return nba_futures
+
+
 def get_team_year_ats_overall(team_id, season):
     content = _get_team_year_ats(team_id=team_id,
                                  season=season)
     ats = _get_type_ats(data=content,
-                        ats_type='atsOverall')
+                    ats_type='atsOverall')
     return ats
 
 
@@ -37,7 +73,7 @@ def get_team_year_ats_favorite(team_id, season):
     content = _get_team_year_ats(team_id=team_id,
                                  season=season)
     ats = _get_type_ats(data=content,
-                        ats_type='atsFavorite')
+                    ats_type='atsFavorite')
     return ats
 
 
