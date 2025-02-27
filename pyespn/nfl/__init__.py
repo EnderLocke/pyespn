295dda82310564217e75e1ea3d5caf4360ba8c58
Zach Stocker <zacharystocker@gmail.com>
2025-02-27 00:32:56 -0600

    added get_team_year_ats_away_underdog

--- pyespn/nfl/__init__.py
+++ pyespn/nfl/__init__.py
@@ -4,6 +4,6 @@ from .orchestration import get_nfl_players_historical_stats
 from .betting import (get_team_year_ats_overall, get_team_year_ats_home_underdog,
                       get_team_year_ats_underdog, get_team_year_ats_home_favorite,
                       get_team_year_ats_away, get_team_year_ats_home, get_team_year_ats_favorite,
-                      get_team_year_ml)
+                      get_team_year_ml, get_team_year_ats_away_underdog)
 from .games import get_game_info
 from .draft import get_draft_pick_data
