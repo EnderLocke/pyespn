00a4431aab5c71ed51b0d27d0918e2abf72ae3c8
Zach Stocker <zacharystocker@gmail.com>
2025-02-26 22:38:13 -0600

    json'd

--- pyespn/nfl/betting.py
+++ pyespn/nfl/betting.py
@@ -5,6 +5,7 @@
 import requests
 import json
 
+
 def get_team_year_ats(team_id, season):
     url = f'http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/{season}/types/2/teams/{team_id}/ats?lang=en&region=us'
     response = requests.get(url)
