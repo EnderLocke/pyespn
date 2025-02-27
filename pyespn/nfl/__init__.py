1c1022c136eee1b103dffc7e51f9949f92ab7461
Zach Stocker <zacharystocker@gmail.com>
2025-02-26 23:14:21 -0600

    added ats funcs

--- pyespn/nfl/__init__.py
+++ pyespn/nfl/__init__.py
@@ -1,5 +1,7 @@
 from .players import get_player_stat_urls, extract_stats_from_url, get_player_info, get_nfl_player_ids
 from .teams import get_season_team_stats, get_team_info
 from .orchestration import get_nfl_players_historical_stats
-from .betting import get_team_year_ats_overall, get_team_year_ml
+from .betting import (get_team_year_ats_overall, get_team_year_ats_home_underdog,
+                      get_team_year_ats_away, get_team_year_ats_home, get_team_year_ats_favorite,
+                      get_team_year_ml)
 from .games import get_game_info
