e27f8c8ecc3ec6fc95d84176e15906ca375d083c
Zach Stocker <zacharystocker@gmail.com>
2025-02-27 22:10:48 -0600

    finished test for recruiting

--- tests/cfb/test_cfb_recruting.py
+++ tests/cfb/test_cfb_recruting.py
@@ -7,6 +7,11 @@ import pytest
 def test_recruit_rankings(test_case):
     content = get_recruiting_rankings(season=test_case['season'])
 
+    test_match = content[test_case['index']]
 
-
-    pass
+    assert test_match['firstName'] == test_case['first_name']
+    assert test_match['lastName'] == test_case['last_name']
+    assert int(test_match['recruitingClass']) == test_case['season']
+    assert test_match['id'] == test_case['id']
+    assert test_match['position'] == test_case['position']
+    assert test_match['grade'] == test_case['grade']
