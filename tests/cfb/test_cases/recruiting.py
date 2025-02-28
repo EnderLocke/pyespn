7b5112f5993d0127239f1dfc0ca6d166a6ed9263
Zach Stocker <zacharystocker@gmail.com>
2025-02-27 22:07:41 -0600

    adding recruiting ranks test cases

---
+++ tests/cfb/test_cases/recruiting.py
@@ -0,0 +1,11 @@
+recruiting_test_cases = [
+    {
+        'season': 2020,
+        'index': 0,
+        'first_name': 'Julian',
+        'last_name': 'Fleming',
+        'id': "229946",
+        'position': 'WR',
+        'grade': 93
+    }
+]
