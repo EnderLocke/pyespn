9015ed9d02f721e8abc858a120047978eba50f43
Zach Stocker <zacharystocker@gmail.com>
2025-02-28 01:14:33 -0600

    added cfb futures

--- pyespn/cfb/betting.py
+++ pyespn/cfb/betting.py
@@ -1,5 +1,5 @@
 from pyespn.utilities import get_type_futures, get_team_id
-from pyespn.cfb.data import cfb_teams_data
+from pyespn.cfb.data import cfb_teams_data, CONFERENCE_MAP
 import requests
 import json
 
@@ -33,3 +33,32 @@ def get_year_cfb_champions_futures(season, provider="DraftKings"):
 
     return futures_list
 
+
+def get_year_conference_champ_futures(season, conference, provider="DraftKings"):
+    """
+
+    :param season:
+    :param conference:
+    :param provider:
+    :return:
+    """
+    content = _get_futures_year(season)
+
+    cfb_futures = get_type_futures(data=content,
+                                   futures_type=CONFERENCE_MAP[conference])
+
+    provider_futures = next(future for future in cfb_futures['futures'] if future['provider']['name'] == provider)
+
+    futures_list = []
+    for item in provider_futures['books']:
+        team_id = get_team_id(item['team']['$ref'])
+        result = next(team for team in cfb_teams_data if team['team_id'] == team_id)
+
+        item_dict = {
+            'team_name': result['team_name'],
+            'team_city': result['team_city'],
+            'champion_future': item['value']
+        }
+        futures_list.append(item_dict)
+
+    return futures_list
