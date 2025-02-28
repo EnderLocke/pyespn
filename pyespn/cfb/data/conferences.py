5d4b9a3387096290704d315ff84983ed8b50e7c4
Zach Stocker <zacharystocker@gmail.com>
2025-02-27 23:23:06 -0600

    adding conference map for betting

---
+++ pyespn/cfb/data/conferences.py
@@ -0,0 +1,7 @@
+CONFERENCE_MAP = {
+    'big12': 'NCAA(F) - Big 12 Conference',
+    'big10': 'NCAA(F) - Big Ten Conference',
+    'acc': 'NCAA(F) - Atlantic Coast Conference',
+    'aac': 'NCAA(F) - American Athletic Conference',
+
+}
