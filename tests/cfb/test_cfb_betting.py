97311a19073df7c24ae5c6ead4ca011ec8e027ac
Zach Stocker <zacharystocker@gmail.com>
2025-02-28 20:02:31 -0600

    added in team id check

--- tests/cfb/test_cfb_betting.py
+++ tests/cfb/test_cfb_betting.py
@@ -22,9 +22,8 @@ def test_conference_futures(test_case):
                                                 conference=test_case['conference'],
                                                 provider=test_case['provider'])
     test_match = content[test_case['index']]
-    #team_id = get_team_id(test_match['team']['$ref'])
 
     assert test_match['team_name'] == test_case['team_name']
     assert test_match['team_city'] == test_case['team_city']
     assert test_match['champion_future'] == test_case['line']
-    #assert team_id == int(test_case['team_id'])
+    assert test_match['team_id'] == int(test_case['team_id'])
