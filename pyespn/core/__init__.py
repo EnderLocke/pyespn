e105646dda708402cd72da28d2365fa1a2699c89
Zach Stocker <zacharystocker@gmail.com>
2025-03-01 14:52:49 -0600

    refactored recruiting api calls into one function to maintain

--- pyespn/core/__init__.py
+++ pyespn/core/__init__.py
@@ -1,3 +1,4 @@
 from .players import (get_player_info_core,
                       get_player_stat_urls_core,
                       get_player_ids_core)
+from .recruiting import get_recruiting_rankings_core
