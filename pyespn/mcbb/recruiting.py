6faf83510cbaf864c9e39c3e344d6376fc298a84
Zach Stocker <zacharystocker@gmail.com>
2025-03-01 14:52:55 -0600

    refactored recruiting api calls into one function to maintain

--- pyespn/mcbb/recruiting.py
+++ pyespn/mcbb/recruiting.py
@@ -1,5 +1,6 @@
 from pyespn.core import get_recruiting_rankings_core
 
+
 def get_recruiting_rankings(season, max_pages=None):
     return get_recruiting_rankings_core(season=season,
                                         league_abbv='mcbb',
