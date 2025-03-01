43fbf61ac57d91c01fb84311387b4f7181d1bc26
Zach Stocker <zacharystocker@gmail.com>
2025-02-28 19:55:50 -0600

    added rest of missing big10 teams

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -1,10 +1,12 @@
 #from pyespn.nba import get_year_nba_champ_futures
 #from pyespn.nfl import get_year_nfl_super_bowl_futures
-from pyespn.cfb import get_recruiting_rankings
+from pyespn.cfb import get_recruiting_rankings, get_year_conference_champ_futures
 
 
 if __name__ == '__main__':
     #data = get_nfl_players_historical_stats(278)
-    data = get_recruiting_rankings(season=2020)
+    data = get_year_conference_champ_futures(season=2023,
+                                             conference='big10',
+                                             provider='Betradar')
     print(data)
     pass
