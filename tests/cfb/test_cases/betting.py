23b7b3b48b6f9f78620ffae65a5dfe4abae59639
Zach Stocker <zacharystocker@gmail.com>
2025-02-28 21:34:34 -0600

    adding test cases for betting

--- tests/cfb/test_cases/betting.py
+++ tests/cfb/test_cases/betting.py
@@ -30,6 +30,36 @@ conference_test_cases = [
         'team_name': 'Longhorns',
         'team_city': 'Texas',
         'id': '251'
+    },
+    {
+        'provider': 'Betradar',
+        'conference': 'acc',
+        'season': 2023,
+        'index': 0,
+        'line': '+139',
+        'team_name': 'Tigers',
+        'team_city': 'Clemson',
+        'id': '228'
+    },
+    {
+        'provider': 'Betradar',
+        'conference': 'aac',
+        'season': 2023,
+        'index': 0,
+        'line': '+211',
+        'team_name': 'Green Wave',
+        'team_city': 'Tulane',
+        'id': '2655'
+    },
+    {
+        'provider': 'Betradar',
+        'conference': 'usa',
+        'season': 2023,
+        'index': 0,
+        'line': '+119',
+        'team_name': 'Hilltoppers',
+        'team_city': 'Western Kentucky',
+        'id': '2655'
     }
 ]
 
