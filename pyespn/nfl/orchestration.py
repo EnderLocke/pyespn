baa06cdf6917667afd36e8da62c2e08566150db8
Zach Stocker <zacharystocker@gmail.com>
2025-03-02 02:55:29 -0600

    removed unneeded cli files

--- pyespn/nfl/orchestration.py
+++
@@ -1,6 +0,0 @@
-from pyespn.core import get_players_historical_stats_core
-
-
-def get_nfl_players_historical_stats(player_id):
-    return get_players_historical_stats_core(player_id=player_id,
-                                             league_abbv='nfl')
