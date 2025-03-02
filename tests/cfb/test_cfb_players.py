e9cfeb199d63adc7290975f0587b7c6aceb525f8
Zach Stocker <zacharystocker@gmail.com>
2025-03-02 03:01:58 -0600

    tests moving to class code

--- tests/cfb/test_cfb_players.py
+++ tests/cfb/test_cfb_players.py
@@ -1,11 +1,12 @@
-from pyespn.cfb import get_player_info
+from pyespn import PYESPN
 from tests.cfb.test_cases.players import test_players
 import pytest
 
 
 @pytest.mark.parametrize("test_case", test_players)
 def test_cfb_player_info(test_case):
-    content = get_player_info(test_case['id'])
+    espn = PYESPN(sport_league='cfb')
+    content = espn.get_player_info(test_case['id'])
     assert content['fullName'] == test_case['full_name']
     assert content['dateOfBirth'] == test_case['dob']
     assert content['type'] == test_case['type']
