40f31f3162120e814878fb34141e64a8671f1c8d
Zach Stocker <zacharystocker@gmail.com>
2025-03-02 19:40:34 -0600

    fixed nba betting test case

--- tests/nba/test_nba_betting.py
+++ tests/nba/test_nba_betting.py
@@ -1,44 +1,14 @@
-from pyespn.nba import get_year_nba_champ_futures, get_year_west_champ_futures, get_year_east_champ_futures
+from pyespn import PYESPN
 import pytest
+from .test_cases.betting import *
 
-nba_champs_test_cases = [
-    {
-        'season': 2023,
-        'provider': 'DraftKings',
-        'team_name': 'Nuggets',
-        'team_city': 'Denver',
-        'line': '+475',
-        'index': 0
-    }
-]
-
-west_champs_test_cases = [
-    {
-        'season': 2023,
-        'provider': 'DraftKings',
-        'team_name': 'Nuggets',
-        'team_city': 'Denver',
-        'line': '+250',
-        'index': 0
-    }
-]
-
-east_champs_test_cases = [
-    {
-        'season': 2023,
-        'provider': 'DraftKings',
-        'team_name': 'Celtics',
-        'team_city': 'Boston',
-        'line': '+210',
-        'index': 0
-    }
-]
+nba_espn = PYESPN('nba')
 
 
 @pytest.mark.parametrize('test_case', nba_champs_test_cases)
 def test_nba_champs_futures(test_case):
-    content = get_year_nba_champ_futures(season=test_case['season'],
-                                         provider=test_case['provider'])
+    content = nba_espn.get_league_year_champion_futures(season=test_case['season'],
+                                                        provider=test_case['provider'])
     test_match = content[test_case['index']]
 
     assert test_match['team_name'] == test_case['team_name']
@@ -46,24 +16,15 @@ def test_nba_champs_futures(test_case):
     assert test_match['champion_future'] == test_case['line']
 
 
-@pytest.mark.parametrize('test_case', west_champs_test_cases)
+@pytest.mark.parametrize('test_case', div_champs_test_cases)
 def test_west_champs_futures(test_case):
-    content = get_year_west_champ_futures(season=test_case['season'],
-                                          provider=test_case['provider'])
+    content = nba_espn.get_league_year_division_champs_futures(season=test_case['season'],
+                                                               division=test_case['division'],
+                                                               provider=test_case['provider'])
     test_match = content[test_case['index']]
 
     assert test_match['team_name'] == test_case['team_name']
     assert test_match['team_city'] == test_case['team_city']
     assert test_match['champion_future'] == test_case['line']
 
-
-@pytest.mark.parametrize('test_case', east_champs_test_cases)
-def test_east_champs_futures(test_case):
-    content = get_year_east_champ_futures(season=test_case['season'],
-                                          provider=test_case['provider'])
-    test_match = content[test_case['index']]
-
-    assert test_match['team_name'] == test_case['team_name']
-    assert test_match['team_city'] == test_case['team_city']
-    assert test_match['champion_future'] == test_case['line']
 
