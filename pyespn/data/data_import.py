7df37435fc14d87c6a336fa416a59b505dd9ae95
Zach Stocker <zacharystocker@gmail.com>
2025-03-02 19:35:39 -0600

    moved the files and forgot to update teh path

--- pyespn/data/data_import.py
+++ pyespn/data/data_import.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-team_lookup_file = 'college_teams_lookup.json'
+team_lookup_file = 'files/college_teams_lookup.json'
 current_dir = os.path.dirname(os.path.abspath(__file__))  # Get directory path
 
 file_path = os.path.join(current_dir, team_lookup_file)  # Get full path
@@ -11,7 +11,7 @@ with open(file_path, "r", encoding="utf-8") as file:
 
 college_teams_data = teams_data_load['teams']
 
-team_lookup_file = 'nfl_teams_lookup.json'
+team_lookup_file = 'files/nfl_teams_lookup.json'
 current_dir = os.path.dirname(os.path.abspath(__file__))  # Get directory path
 
 file_path = os.path.join(current_dir, team_lookup_file)  # Get full path
@@ -21,7 +21,7 @@ with open(file_path, "r", encoding="utf-8") as file:
 
 nfl_teams_data = teams_data_load['teams']
 
-team_lookup_file = 'nba_teams_lookup.json'
+team_lookup_file = 'files/nba_teams_lookup.json'
 current_dir = os.path.dirname(os.path.abspath(__file__))  # Get directory path
 
 file_path = os.path.join(current_dir, team_lookup_file)  # Get full path
