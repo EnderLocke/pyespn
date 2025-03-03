9df87f8d7a7dfa41003c4afa0b61f9c31dd88ee8
Zach Stocker <zacharystocker@gmail.com>
2025-03-02 19:42:44 -0600

    fixed nfl betting test cases

--- tests/nfl/test_nfl_betting.py
+++ tests/nfl/test_nfl_betting.py
@@ -1,9 +1,12 @@
+from pyespn import PYESPN
 from pyespn.nfl import (get_team_year_ats_overall, get_team_year_ml,
                         get_year_nfc_division_champ_futures,
                         get_year_afc_division_champ_futures,
                         get_year_nfl_super_bowl_futures)
 from tests.nfl.test_cases.betting import *
 import pytest
+
+nfl_espn = PYESPN('nfl')
 
 ats_overall_test_cases = [
     {
@@ -28,8 +31,8 @@ def test_ats_overall(test_case):
 
 @pytest.mark.parametrize("test_case", super_bowl_test_cases)
 def test_super_bowl_futures(test_case):
-    content = get_year_nfl_super_bowl_futures(season=test_case['season'],
-                                              provider=test_case['provider'])
+    content = nfl_espn.get_league_year_champion_futures(season=test_case['season'],
+                                                        provider=test_case['provider'])
     test_match = content[test_case['index']]
 
     assert test_match['team_name'] == test_case['team_name']
@@ -37,23 +40,11 @@ def test_super_bowl_futures(test_case):
     assert test_match['champion_future'] == test_case['line']
 
 
-@pytest.mark.parametrize("test_case", afc_test_cases)
+@pytest.mark.parametrize("test_case", div_test_cases)
 def test_afc_div_futures(test_case):
-    content = get_year_afc_division_champ_futures(season=test_case['season'],
-                                                  division=test_case['division'],
-                                                  provider=test_case['provider'])
-    test_match = content[test_case['index']]
-
-    assert test_match['team_name'] == test_case['team_name']
-    assert test_match['team_city'] == test_case['team_city']
-    assert test_match['champion_future'] == test_case['line']
-
-
-@pytest.mark.parametrize("test_case", nfc_test_cases)
-def test_nfc_div_futures(test_case):
-    content = get_year_nfc_division_champ_futures(season=test_case['season'],
-                                                  division=test_case['division'],
-                                                  provider=test_case['provider'])
+    content = nfl_espn.get_league_year_division_champs_futures(season=test_case['season'],
+                                                               division=test_case['division'],
+                                                               provider=test_case['provider'])
     test_match = content[test_case['index']]
 
     assert test_match['team_name'] == test_case['team_name']
