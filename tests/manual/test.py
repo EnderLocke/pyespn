d5970491ad945a3a46af7a56c70e16a4a72f3cbf
Zach Stocker <zacharystocker@gmail.com>
2025-03-03 21:27:45 -0600

    manual test when needing to debug

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -1,12 +1,9 @@
-#from pyespn.nba import get_year_nba_champ_futures
-#from pyespn.nfl import get_year_nfl_super_bowl_futures
-from pyespn.cfb import get_recruiting_rankings, get_year_conference_champ_futures
+from pyespn import PYESPN
 
 
 if __name__ == '__main__':
     #data = get_nfl_players_historical_stats(278)
-    data = get_year_conference_champ_futures(season=2023,
-                                             conference='big10',
-                                             provider='Betradar')
+    espn = PYESPN('nba')
+    data = espn.get_awards(season=2023)
     print(data)
     pass
