fbf5b7c6b0765af6a248059be385ba3bc28ff1e7
Zach Stocker <zacharystocker@gmail.com>
2025-03-04 01:32:41 -0600

    added nascar

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -3,7 +3,7 @@ from pyespn import PYESPN
 
 if __name__ == '__main__':
     #data = get_nfl_players_historical_stats(278)
-    espn = PYESPN('nba')
-    data = espn.get_awards(season=2023)
+    espn = PYESPN('nascar')
+    data = espn.get_standings(season=2024, standings_type='driver')
     print(data)
     pass
