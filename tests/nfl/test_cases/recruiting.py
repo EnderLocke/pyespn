5a212e320a5741f84efd6bd60401c4d9af3f122e
Zach Stocker <zacharystocker@gmail.com>
2025-03-15 00:24:00 -0500

    adding nfl recruiting tesat case (it should fail)

---
+++ tests/nfl/test_cases/recruiting.py
@@ -0,0 +1,13 @@
+recruiting_test_cases = [
+    {
+        'season': 2020,
+        'index': 0,
+        'first_name': 'Julian',
+        'last_name': 'Fleming',
+        'id': "229946",
+        'position': 'WR',
+        'grade': 93,
+        'max_pages': 10,
+        'rank': 1
+    }
+]
