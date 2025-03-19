4d1c6642da1d282a61fa90379892db4faf1f5a7b
Zach Stocker <zacharystocker@gmail.com>
2025-03-19 12:50:35 -0500

    adding classes to their own structure. creating player class

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -1,9 +1,18 @@
 from pyespn import PYESPN
+player_id = 278 # Jimmy Smith, Goat
+season = 2021
+pick = 8
+pick_round = 2
 
 
 if __name__ == '__main__':
     #data = get_nfl_players_historical_stats(278)
-    espn = PYESPN('nascar')
-    data = espn.get_standings(season=2024, standings_type='driver')
+    espn = PYESPN('nba')
+    data = espn.get_draft_pick_data(pick_round=pick_round,
+                                    pick=pick,
+                                    season=season)
+    awards = espn.get_awards(season=season)
+    print(espn.BETTING_PROVIDERS)
+    #colors = espn.get_team_colors(team_id=30)
     print(data)
     pass
