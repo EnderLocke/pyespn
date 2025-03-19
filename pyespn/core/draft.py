2ca47a2bd45aca0a4b9647a9ba1aee0b4aa7158b
Zach Stocker <zacharystocker@gmail.com>
2025-03-19 14:26:45 -0500

    fixed v and added espn fetch function

--- pyespn/core/draft.py
+++ pyespn/core/draft.py
@@ -4,8 +4,8 @@ from pyespn.data.version import espn_api_version as v
 
 def get_draft_pick_data_core(pick_round, pick, season, league_abbv):
     api_info = lookup_league_api_info(league_abbv=league_abbv)
-    url = f'http://sports.core.api.espn.com/v2/sports/{api_info["sport"]}/leagues/{api_info["league"]}/seasons/{season}/draft/rounds/{pick_round}/picks/{pick}'
-    response = requests.get(url)
-    content = json.loads(response.content)
+    url = f'http://sports.core.api.espn.com/{v}/sports/{api_info["sport"]}/leagues/{api_info["league"]}/seasons/{season}/draft/rounds/{pick_round}/picks/{pick}'
+    content = fetch_espn_data(url)
+
     return content
 
