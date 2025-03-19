1314bc61037b38ae2af16573264519a816430cc1
Zach Stocker <zacharystocker@gmail.com>
2025-03-19 13:39:34 -0500

    adding in function to build out team class with data veresus json call

--- pyespn/classes/team.py
+++ pyespn/classes/team.py
@@ -14,7 +14,8 @@ class Team:
         location (str): Geographic location of the team (e.g., "Los Angeles").
     """
 
-    def __init__(self, espn_instance, team_id, name, abbreviation, location):
+    def __init__(self, espn_instance, team_id=None, name=None,
+                 abbreviation=None, location=None, team_json=None):
         """
         Initializes a Team instance.
 
@@ -30,6 +31,55 @@ class Team:
         self.name = name
         self.abbreviation = abbreviation
         self.location = location
+        if team_json:
+            self.team_json = team_json
+        else:
+            self.team_json = None
+
+        if self.team_json:
+            self._load_team_data()
+
+    def _load_team_data(self):
+        self.id = self.team_json.get("id")
+        self.guid = self.team_json.get("guid")
+        self.uid = self.team_json.get("uid")
+        self.location = self.team_json.get("location")
+        self.name = self.team_json.get("name")
+        self.nickname = self.team_json.get("nickname")
+        self.abbreviation = self.team_json.get("abbreviation")
+        self.display_name = self.team_json.get("displayName")
+        self.short_display_name = self.team_json.get("shortDisplayName")
+        self.primary_color = self.team_json.get("color")
+        self.alternate_color = self.team_json.get("alternateColor")
+        self.is_active = self.team_json.get("isActive")
+        self.is_all_star = self.team_json.get("isAllStar")
+
+        self.logos = [logo.get("href") for logo in self.team_json.get("logos", [])]
+        self.venue_name = self.team_json.get("venue", {}).get("fullName", "Unknown Venue")
+        self.venue_city = self.team_json.get("venue", {}).get("address", {}).get("city", "Unknown")
+        self.venue_state = self.team_json.get("venue", {}).get("address", {}).get("state", "Unknown")
+        self.venue_grass = self.team_json.get("venue", {}).get("grass", "Unknown")
+        self.venue_indoors = self.team_json.get("venue", {}).get("indoor", "Unknown")
+        self.venue_imgs = self.team_json.get("venue", {}).get("images", [])
+
+        self.links = {link["rel"][0]: link["href"] for link in self.team_json.get("links", []) if "rel" in link}
+
+    def get_team_colors(self):
+        return {
+            'primary_color': self.primary_color,
+            'alt_color': self.alternate_color
+        }
+
+    def get_home_venue(self):
+
+        return {
+            'name': self.venue_name,
+            'state': self.venue_state,
+            'city': self.venue_city,
+            'grass': self.venue_grass,
+            'indoor': self.venue_indoors,
+            'img_url': self.venue_imgs
+        }
 
     def get_league(self):
         """
--- tests/manual/test.py
+++ tests/manual/test.py
@@ -7,11 +7,9 @@ pick_round = 2
 
 if __name__ == '__main__':
     #data = get_nfl_players_historical_stats(278)
-    espn = PYESPN('nba')
-    data = espn.get_draft_pick_data(pick_round=pick_round,
-                                    pick=pick,
-                                    season=season)
-    awards = espn.get_awards(season=season)
+    espn = PYESPN('nfl')
+    data = espn.get_player_info(player_id=player_id)
+    #awards = espn.get_awards(season=season)
     print(espn.BETTING_PROVIDERS)
     #colors = espn.get_team_colors(team_id=30)
     print(data)
