a0b3e32c6db4291e852d889959754f77b21ef598
Zach Stocker <zacharystocker@gmail.com>
2025-03-19 19:22:44 -0500

    added skeleton for the league class

--- pyespn/classes/__init__.py
+++ pyespn/classes/__init__.py
@@ -2,3 +2,4 @@ from .team import Team
 from .player import Player
 from .event import Event
 from .venue import Venue
+from .league import League
---
+++ pyespn/classes/league.py
@@ -0,0 +1,6 @@
+# todo create a league version of the api that can pull a certain amount of data
+
+class League:
+
+    def __init__(self):
+        pass
