6f1e9034705edc011c246eeb9091d38931cf68d5
Zach Stocker <zacharystocker@gmail.com>
2025-03-19 19:31:57 -0500

    adding in event class

--- pyespn/core/games.py
+++ pyespn/core/games.py
@@ -1,15 +1,14 @@
 from pyespn.utilities import lookup_league_api_info, fetch_espn_data
 from pyespn.data.version import espn_api_version as v
-import requests
-import json
+from pyespn.classes import Event
 
 
 def get_game_info_core(event_id, league_abbv):
     api_info = lookup_league_api_info(league_abbv=league_abbv)
     url = f'http://sports.core.api.espn.com/{v}/sports/{api_info["sport"]}/leagues/{api_info["league"]}/events/{event_id}?lang=en&region=us'
     content = fetch_espn_data(url)
-
-    return content
+    current_event = Event(event_json=content)
+    return content, current_event
 
 
 def get_events_by_team(team_id, season, league_abbv):
