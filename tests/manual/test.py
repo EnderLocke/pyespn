66bba7f71be532fd76c4039ae106cb68863eb54b
Zach Stocker <zacharystocker@gmail.com>
2025-03-29 00:57:53 -0500

    a test

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -1,16 +1,16 @@
 from pyespn import PYESPN
 player_id = 278 # Jimmy Smith, Goat
-season = 2021
+season = 2024
 pick = 8
 pick_round = 2
 
 
 if __name__ == '__main__':
     #data = get_nfl_players_historical_stats(278)
-    espn = PYESPN('f1')
-    data = espn.get_team_info(team_id=1)
+    nfl_espn = PYESPN('nfl')
+    nba_espn = PYESPN('nba')
+    data = nfl_espn.get_all_seasons_futures(season=season)
     #awards = espn.get_awards(season=season)
-    print(espn.BETTING_PROVIDERS)
     #colors = espn.get_team_colors(team_id=30)
     print(data)
     pass
