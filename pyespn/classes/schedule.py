f2d9728012fc20cbade1ff3904f6818340f69ae3
Zach Stocker <zacharystocker@gmail.com>
2025-03-29 19:28:00 -0500

    added in schedule api call

--- pyespn/classes/schedule.py
+++ pyespn/classes/schedule.py
@@ -1,27 +1,79 @@
 from pyespn.core.decorators import validate_json
+from pyespn.utilities import fetch_espn_data, get_schedule_type, get_an_id
 from pyespn.classes import Event
 
 
-@validate_json("schedule_json")
 class Schedule:
 
-    def __init__(self, espn_instance, schedule_json):
-        self.schedule_json = schedule_json
+    def __init__(self, espn_instance, schedule_list):
+        self.schedule_list = schedule_list
         self.espn_instance = espn_instance
+        self.schedule_type = None
+
+        schedule_type_id = get_schedule_type(self.schedule_list[0])
+
+        if schedule_type_id == 1:
+            self.schedule_type = 'preseason'
+        elif schedule_type_id == 2:
+            self.schedule_type = 'regular'
+        elif schedule_type_id == 3:
+            self.schedule_type = 'post'
+
+        self._set_schedule_data()
+
+    def __repr__(self):
+        """
+        Returns a string representation of the schedule instance.
+
+        Returns:
+            str: A formatted string with the schedule.
+        """
+        return f"<Schedule | {self.schedule_type})>"
+
 
     def _set_schedule_data(self):
-        # todo this is 1 preseason 2 regular 3 post
-        self.schedule_type = 'regular'
+
+        for week_url in self.schedule_list:
+            api_url = week_url.split('?')[0] + f'/events'
+            week_content = fetch_espn_data(api_url)
+            # todo here i need to capture the data
+            week_pages = week_content.get('pageCount')
+
+            for week_page in range(1, week_pages + 1):
+                weekly_url = api_url + f'?page={week_page}'
+                this_week_content = fetch_espn_data(weekly_url)
+                event_urls = []
+                for event in this_week_content.get('items', []):
+                    event_urls.append(event.get('$ref'))
+                self.weeks = Week(espn_instance=self.espn_instance,
+                                  week_list=event_urls)
 
 
-@validate_json("week_json")
 class Week:
 
-    def __init__(self, espn_instance, week_json):
+    def __init__(self, espn_instance, week_list):
         self.espn_instance = espn_instance
-        self.week_json = week_json
+        self.week_list = week_list
+        self.events = []
+        self.week_number = None
 
+        self.week_number = get_an_id(url=self.week_list[0],
+                                     slug='week')
 
-    def _set_schedule_data(self):
-        pass
+        self._set_week_data()
+
+    def __repr__(self):
+        """
+        Returns a string representation of the week instance.
+
+        Returns:
+            str: A formatted string with the week.
+        """
+        return f"<Week | {self.week_number})>"
+
+    def _set_week_data(self):
+        for event in self.week_list:
+            event_content = fetch_espn_data(event.get('$ref'))
+            self.events.append(Event(event_json=event_content,
+                                     espn_instance=self.espn_instance))
 
--- pyespn/core/schedule.py
+++ pyespn/core/schedule.py
@@ -31,22 +31,7 @@ def get_regular_season_schedule_core(league_abbv, espn_instance, season):
         for item in page_content.get('items', []):
             weeks_urls.append(item.get('$ref'))
 
-    for week_url in weeks_urls:
-        api_url = week_url.split('?')[0] + f'/events'
-        week_content = fetch_espn_data(api_url)
-        # todo here i need to capture the data
-        week_pages = week_content.get('pageCount')
+    schedule = Schedule(schedule_list=weeks_urls,
+                        espn_instance=espn_instance)
 
-        for week_page in range(1, week_pages + 1):
-            weekly_url = api_url + f'?page={week_page}'
-            this_week_content = fetch_espn_data(weekly_url)
-            event_list = []
-
-            for event in this_week_content.get('items', []):
-                event_content = fetch_espn_data(event.get('$ref'))
-                event_list.append()
-                pass
-            pass
-        pass
-
-    return season_schedule
+    return schedule
