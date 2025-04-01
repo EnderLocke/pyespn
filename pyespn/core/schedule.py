e1eef3c626eb3cb0d65872dc9b3916f153f48616
Zach Stocker <zacharystocker@gmail.com>
2025-04-01 05:47:18 -0500

    added szn type as param, defau;lts to 2 so nthing breaks

--- pyespn/core/schedule.py
+++ pyespn/core/schedule.py
@@ -18,10 +18,10 @@ def get_weekly_schedule_core(league_abbv, espn_instance, season, week):
 
 
 # todo 1 is preseason 2 is regular season and 3 is postseason
-def get_regular_season_schedule_core(league_abbv, espn_instance, season):
+def get_regular_season_schedule_core(league_abbv, espn_instance, season, season_type='2'):
     # right now this only works fornfl, probably college football too
     api_info = lookup_league_api_info(league_abbv=league_abbv)
-    url = f'http://sports.core.api.espn.com/{v}/sports/{api_info["sport"]}/leagues/{api_info["league"]}/seasons/{season}/types/2/weeks'
+    url = f'http://sports.core.api.espn.com/{v}/sports/{api_info["sport"]}/leagues/{api_info["league"]}/seasons/{season}/types/{season_type}/weeks'
     content = fetch_espn_data(url)
 
     pages = content.get('pageCount')
