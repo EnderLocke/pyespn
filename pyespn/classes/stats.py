c1759f9a70984b524d506dcc92b994de5c194042
Zach Stocker <zacharystocker@gmail.com>
2025-04-03 05:15:55 -0500

    adding in stats class

--- pyespn/classes/stats.py
+++ pyespn/classes/stats.py
@@ -2,8 +2,9 @@
 
 class Stats:
 
-    def __init__(self):
+    def __init__(self, stat_json, espn_isntance):
+        self.stat_json = stat_json
+        self.espn_instance = espn_isntance
 
+    def _set_stats_data(self):
         pass
-
-
