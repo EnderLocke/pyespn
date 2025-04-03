5d1bdbefdb66c2995868a0bff4c3969e585e2ac3
Zach Stocker <zacharystocker@gmail.com>
2025-04-02 23:33:33 -0500

    my manual test

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -6,12 +6,9 @@ pick_round = 2
 
 
 if __name__ == '__main__':
-    #data = get_nfl_players_historical_stats(278)
-    #nfl_espn = PYESPN('nfl')
-    espn = PYESPN('nfl')
-    #cfb_espn = PYESPN('cfb')
-    #cfb_espn.load_regular_season_schedule(season=season)
-    espn.teams[0].load_season_roster(season=season)
+
+    espn = PYESPN('nba')
+    espn.load_season_rosters(season=season)
     espn.load_regular_season_schedule(season=season)
     #awards = espn.get_awards(season=season)
     #colors = espn.get_team_colors(team_id=30)
