9be431d1ce7dd4475a3fb8e90e42807dace5751d
Zach Stocker <zacharystocker@gmail.com>
2025-04-08 03:52:12 -0500

    added rank to stat leader class

--- pyespn/classes/stat.py
+++ pyespn/classes/stat.py
@@ -126,8 +126,38 @@ class Record:
 
 
 class LeaderCategory:
+    """
+    Represents a category of statistical leaders for a given season.
+
+    The LeaderCategory class is responsible for storing and managing information
+    about a specific leader category, such as the category's name, abbreviation,
+    and the athletes who are the statistical leaders in that category for the
+    specified season. The data is loaded from a given JSON object, and the class
+    provides methods to represent and interact with this data.
+
+    Attributes:
+        leader_cat_json (dict): The JSON data containing information about the leader category.
+        espn_instance (object): The instance of the ESPN-related class for interacting with ESPN data.
+        athletes (dict): A dictionary holding athletes (as instances of the Leader class) for each season.
+        season (str or int): The season for which the leader category data is relevant.
+        abbreviation (str): The abbreviation of the leader category.
+        name (str): The name of the leader category.
+        display_name (str): The display name of the leader category.
+
+    Methods:
+        __repr__(): Returns a string representation of the LeaderCategory instance.
+        _load_leaders_data(): Loads the leader data from the provided JSON and initializes the class attributes.
+    """
 
     def __init__(self, leader_cat_json, espn_instance, season):
+        """
+        Initializes a LeaderCategory instance with the given data.
+
+        Args:
+            leader_cat_json (dict): The JSON data for the leader category.
+            espn_instance (object): An instance of the ESPN class for interacting with ESPN data.
+            season (str or int): The season the leader category is related to.
+        """
         self.leader_cat_json = leader_cat_json
         self.espn_instance = espn_instance
         self.athletes = {}
@@ -144,23 +174,35 @@ class LeaderCategory:
         return f"<LeaderCategory | {self.season}-{self.display_name}>"
 
     def _load_leaders_data(self):
+        """
+        Loads the leaders' data from the provided JSON.
+
+        This method extracts relevant information (such as abbreviation, name,
+        display name, and athletes) from the `leader_cat_json` and populates
+        the corresponding attributes. It also creates instances of the `Leader`
+        class for each athlete and stores them in the `athletes` dictionary,
+        indexed by season.
+        """
         self.abbreviation = self.leader_cat_json.get('abbreviation')
         self.name = self.leader_cat_json.get('name')
         self.abbreviation = self.leader_cat_json.get('abbreviation')
         self.display_name = self.leader_cat_json.get('displayName')
         all_athletes = []
+        rank = 1
         for ath in self.leader_cat_json.get('leaders', []):
             all_athletes.append(Leader(leader_json=ath,
-                                       espn_instance=self.espn_instance))
-            pass
+                                       espn_instance=self.espn_instance,
+                                       rank=rank))
+            rank += 1
         self.athletes[self.season] = all_athletes
 
 
 class Leader:
 
-    def __init__(self, leader_json, espn_instance):
+    def __init__(self, leader_json, espn_instance, rank):
         self.leader_json = leader_json
         self.espn_instance = espn_instance
+        self.rank = rank
         self.athlete = None
         self.team = None
         self._load_leader_data()
