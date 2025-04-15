4ed541e601113e38747d0439337f87e0002b3146
Zach Stocker <zacharystocker@gmail.com>
2025-04-14 22:53:14 -0500

    my test

--- tests/manual/test.py
+++ tests/manual/test.py
@@ -7,8 +7,8 @@ pick_round = 2
 
 if __name__ == '__main__':
 
-    espn = PYESPN('nba')
-    espn.load_season_schedule(season=season, load_play_in=True, load_game_odds=True)
+    espn = PYESPN('mlb')
+    espn.load_season_schedule(season=season, load_game_odds=True)
     espn.league.load_regular_season_schedule(season=season, load_game_odds=True)
     #espn.load_season_rosters(season=season)
     #espn.teams[0].load_season_roster_box_score(season=season)
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     #espn.load_standings(season=season)
     #espn.load_season_league_stat_leaders(season=2024)
     #espn.load_season_team_stats(season=2024)
-    espn.league.load_regular_season_schedule(season=season, load_game_odds=True)
     #stats = espn.get_players_historical_stats(player_id=player_id)
     #espn.load_athletes(season=season)
     #espn.load_regular_season_schedule(season=season)
